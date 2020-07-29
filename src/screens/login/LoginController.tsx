8e0f9444910aaa201b7547ac1793985fec3a6e21
Ahmed Elshaabany <ahmed@savedroid.de>
2020-07-29 02:49:42 +0200

    * Edit README.md

--- src/screens/login/LoginController.tsx
+++ src/screens/login/LoginController.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Pressable, Text, StyleSheet } from 'react-native';
-import { appStyles } from 'commons/styles';
+import React, {useState} from 'react';
+import {SafeAreaView, Pressable, Text, StyleSheet} from 'react-native';
+import {appStyles} from 'commons/styles';
 import CustomTextBox from 'components/CustomTextBox';
 import AnimatedView from 'components/AnimatedView';
 import LinearGradient from 'react-native-linear-gradient';
 
-const LoginController = ({ navigation }: any) => {
+const LoginController = ({navigation}: any) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     return (
         <LinearGradient
-            style={{ flex: 1 }}
+            style={{flex: 1}}
             colors={['#FF00FF', '#7F00E0']}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
+            start={{x: 0, y: 0}}
+            end={{x: 1, y: 1}}
             locations={[0.1, 0.7]}>
             <SafeAreaView style={[appStyles.safeArea]}>
                 <AnimatedView delay={1000}>
@@ -29,7 +29,7 @@ const LoginController = ({ navigation }: any) => {
                     }
                 </AnimatedView>
 
-                <AnimatedView delay={1800} style={{ marginTop: 30 }}>
+                <AnimatedView delay={1800} style={{marginTop: 30}}>
                     {
                         <CustomTextBox
                             placeholder={'Password'}
@@ -55,7 +55,7 @@ const LoginController = ({ navigation }: any) => {
 
 
                 <AnimatedView delay={2000} style={styles.button}>
-                    <Pressable style={({ pressed }) => [
+                    <Pressable style={({pressed}) => [
                         {
                             backgroundColor: pressed ? 'yellow' : 'transparent',
                             borderRadius: 8,
@@ -64,8 +64,8 @@ const LoginController = ({ navigation }: any) => {
                         <LinearGradient
                             style={styles.gradientContainer}
                             colors={['#7F00E0', '#FF00FF']}
-                            start={{ x: 0, y: 0 }}
-                            end={{ x: 1, y: 1 }}
+                            start={{x: 0, y: 0}}
+                            end={{x: 1, y: 1}}
                             locations={[0.1, 0.9]}>
                             <Text style={styles.buttonText}>Press me!</Text>
                         </LinearGradient>
